2e683fea85ca32ca312ce708d9c1dcad7f5c1b29
Kevin Raoofi <nullindev@gmail.com>
2017-10-08 05:08:34 -0400

    integrated search with standard format + image lookup

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,32 @@ const {uploadImage, detectFood} = require('../services/image_handle');
 
 const service = new SpiceService();
 
+function ingredientize(recipes) {
+  let map = {};
+
+  recipes.forEach((recipe) => {
+    recipe.ingredients
+    .filter((e) => e.ingredientProductUpc)
+    .forEach((ingredient) => {
+      let name = ingredient.ingredientName;
+      if(!map[name]) {
+        map[name] = {
+          name: ingredient.ingredientName,
+          recipes: [],
+          ingredientProductLink: recipe.ingredientProductLink
+        };
+      }
+
+      map[name].recipes = [...map[name].recipes, {
+        title: recipe.title,
+        custom_url: recipe.custom_url
+      }];
+    });
+  });
+
+  return Object.values(map);
+}
+
 router.use(fileUpload());
 
 router.get('/', function(req, res){
@@ -19,7 +45,8 @@ router.get('/search/:name', function(req, res, next) {
       throw err;
     }
 
-    res.json(result.rows.map((row) => row.doc));
+    const transformed = ingredientize(result.rows.map((row) => row.doc));
+    res.json(transformed);
   });
 });
 
@@ -35,18 +62,34 @@ router.post('/imageSuggest', function(req, res) {
     if (err) {
       return res.status(500).send(err);
     }
-    res.json(data);
-  });
 
-  /*image.mv('./public/uploads/uploaded.jpg', (err) => {
-    if(err) {
-      return res.status(500).send(err);
+    let best = data.Labels.slice(0, 1);
+
+    if(best.length !== 1) {
+      return res.json({
+          query: '',
+          confidence: 0,
+          result: []
+      });
     }
 
-    res.send('File uploaded!');
-  });
-  */
+    best = best[0];
 
+    service.search(best.Name, (err, result) => {
+      if(err) {
+        throw err;
+      }
+
+      const transformed = ingredientize(result.rows.map((row) => row.doc));
+
+      console.log(best);
+      res.json({
+        query: best.Name,
+        confidence: best.Confidence,
+        result: transformed
+      });
+    });
+  });
 });
 
 module.exports = router;
