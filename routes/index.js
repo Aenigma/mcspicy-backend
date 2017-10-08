8e5e461a6be24e7406d1c0bb61d2080bad6a75ee
Kevin Raoofi <nullindev@gmail.com>
2017-10-08 02:12:26 -0400

    file upload demo working

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const fileUpload = require('express-fileupload');
 const router = express.Router()
 
 const {SpiceService} = require('../services/spiceservice')
 
 const service = new SpiceService();
+
+router.use(fileUpload());
 
 router.get('/', function(req, res){
   res.json({ title: 'Cloudant Boiler Plate' });
@@ -17,6 +20,24 @@ router.get('/search/:name', function(req, res, next) {
 
     res.json(result.rows.map((row) => row.doc));
   });
+});
+
+router.post('/imageSuggest', function(req, res) {
+  if (!req.files) {
+    return res.status(400).send('No files were uploaded.');
+  }
+
+  const image = req.files.image;
+  //const buffer = image.data;
+
+  image.mv('./public/uploads/uploaded.jpg', (err) => {
+    if(err) {
+      return res.status(500).send(err);
+    }
+
+    res.send('File uploaded!');
+  });
+
 });
 
 module.exports = router;
